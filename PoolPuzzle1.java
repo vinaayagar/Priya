83a49ac34f93ebf7873379b5cdc7040c3cb55930
Priya <39764559+vinaayagar@users.noreply.github.com>
2019-05-09 12:01:14 +0530

    First commit
    1st Description

---
+++ PoolPuzzle1.java
@@ -0,0 +1,31 @@
+class PoolPuzzle1
+{
+public static void main(String[] args)
+{
+int x=0;
+while(x<4)
+{
+System.out.print("a");
+if(x<1)
+{
+System.out.print(" ");
+}
+System.out.print("n");
+if(x>1)
+{
+System.out.print(" oyster ");
+x=x+2;
+}
+if(x=1)
+{
+System.out.print("nois");
+}
+if(x<1)
+{
+System.out.print("oise");
+}
+System.out.print(" ");
+x=x+1;
+}
+}
+}
